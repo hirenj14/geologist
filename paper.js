c1654ea33c7187c9706034fd18a7962c27ea765d
hirenj14 <76769318+hirenj14@users.noreply.github.com>
2021-01-29 11:27:43 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,17 @@
+class Waste{
+    constructor(x, y, radius){
+        var options = {
+            restitution: 0.3,
+            density: 1.2,
+            friction: 0.5
+        }
+        this.body = Bodies.circle(x, y, radius, options);
+        this.radius = radius;
+        World.add(world, this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        fill("white");
+        ellipse(pos.x, pos.y, this.radius);
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,57 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ground, dustbin, dustbin2, dustbin3;
+var paper;
+
+var gameState = "dirty";
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	dustbin = new Dustbin2(550, 550, 240, 20);
+	dustbin2 = new Dustbin2(440, 490, 20, 100);
+	dustbin3 = new Dustbin2(660, 490, 20, 100);
+	ground = new Ground(400, 570, 800, 20);
+	paper = new Waste(200 ,200, 30);
+
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(135,206,235);
+
+  dustbin.display();
+  dustbin2.display();
+  dustbin3.display();
+  ground.display();
+  paper.display();
+
+  drawSprites();
+ 
+}
+function keyPressed(){
+	if(keyCode === UP_ARROW){
+		Matter.Body.applyForce(paper.body, paper.body.position,{x:100,y:-150});
+	}
+}
+
+
+
+
